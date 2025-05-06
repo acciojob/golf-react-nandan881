040a2418822d429b6d40839135c41cac6a6b97d5
nandan kumar <kumarnandan551@gmail.com>
2025-05-06 14:04:55 +0530

    golfgame

--- src/components/App.js
+++ src/components/App.js
@@ -5,27 +5,31 @@ class GolfGame extends Component {
     super(props);
     this.state = {
       showBall: false,
-      position: 0
+      position: 0,
     };
   }
 
   componentDidMount() {
+    // Listen for right arrow key press to move the ball
     document.addEventListener('keydown', this.handleKeyDown);
   }
 
   componentWillUnmount() {
+    // Clean up event listener when component unmounts
     document.removeEventListener('keydown', this.handleKeyDown);
   }
 
   handleKeyDown = (event) => {
+    // If Right Arrow key is pressed, move the ball 5px to the right
     if (event.keyCode === 39 && this.state.showBall) {
       this.setState((prevState) => ({
-        position: prevState.position + 5
+        position: prevState.position + 5,
       }));
     }
   };
 
   buttonClickHandler = () => {
+    // Show the ball when the start button is clicked
     this.setState({ showBall: true });
   };
 
@@ -45,7 +49,7 @@ class GolfGame extends Component {
               width: '50px',
               height: '50px',
               borderRadius: '50%',
-              backgroundColor: 'green'
+              backgroundColor: 'green',
             }}
           ></div>
         )}
